b08d1a231a14f52b390e59bc7cce3f9f459f1585
Elbek <erikbro792@gmail.com>
2024-10-02 19:44:16 +0500

    yangi sayt

---
+++ accaunt/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Category, Product, ProductImages, MyUser, Comment
+
+
+admin.site.register(Category)
+admin.site.register(ProductImages)
+admin.site.register(Product)
+admin.site.register(MyUser)
+admin.site.register(Comment)
+# Register your models here.
